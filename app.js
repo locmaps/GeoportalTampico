de121ce11d593e22315606d6010bdd103e899e06
locmaps <locmapsinfo@gmail.com>
2026-05-28 16:08:55 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -664,7 +664,7 @@ function attachEventListeners() {
   });
 
   document.getElementById('btnHelp').addEventListener('click', () => openModal('helpModal'));
-  document.getElementById('btnAbout').addEventListener('click', () => openModal('aboutModal'));
+  document.getElementById('btnAbout')?.addEventListener('click', () => openModal('aboutModal'));
   document.getElementById('btnShare').addEventListener('click', handleShare);
 
   document.getElementById('closeHelpBtn').addEventListener('click', () => closeModal('helpModal'));
